661af97776a0f053310ba998c9cac551d15b2ef8
LeoHoward123 <102544537+LeoHoward123@users.noreply.github.com>
2026-01-29 16:16:08 +0700

    Update route.js

--- app/api/attendance/route.js
+++ app/api/attendance/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { db } from '@/lib/firebaseAdmin';
+import { db } from '../../../lib/firebaseAdmin';
 import * as admin from 'firebase-admin';
 
 export async function POST(req) {
